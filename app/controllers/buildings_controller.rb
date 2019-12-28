762fd6db526fd5fc6c5d31ed3d37c26e0a6154ea
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-27 21:16:39 -0500

    adds forms for buildings

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,5 +1,5 @@
 class BuildingsController < ApplicationController
-    before_action :get_building, only: [:show]
+    before_action :get_building, only: [:show, :edit, :update]
 
 
     def index
@@ -10,7 +10,41 @@ class BuildingsController < ApplicationController
 
     end
 
+    def new
+        @building = Building.new
+    end
+
+    def create
+        @building = Building.new(building_params)
+
+        if @building.save
+            redirect_to building_path(@building)
+        else
+            render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+        building_params.each do |attribute, value|
+            @building.send("#{attribute}=", value)
+        end
+
+        if @building.save
+            redirect_to building_path(@building)
+        else
+            render :edit
+        end
+    end
+
     private
+
+    def building_params
+        params.require(:building).permit(:name, :owner, :leasee, :district_id)
+    end
 
     def get_building
         @building = Building.find(params.require(:id))
--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,5 @@
 class Building < ApplicationRecord
   belongs_to :district
+
+  validates :name, :owner, :leasee, :district_id, presence: true
 end
